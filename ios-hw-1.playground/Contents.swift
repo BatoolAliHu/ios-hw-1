6fd5ea197a8202c9548f65bb8f8a97c208e570e6
BatoolAliHussain <66515156+BatoolAliHu@users.noreply.github.com>
2020-06-13 22:50:03 +0300

    done
    تم الانتهاء من عمل الواجب

--- ios-hw-1.playground/Contents.swift
+++ ios-hw-1.playground/Contents.swift
@@ -5,12 +5,12 @@
  
  */
 
-let name = "Ahmad"
+var name = "Ahmad"
 
 name = "Salem"
 
 
-var myWeight = 60
+var myWeight = 60.5
 var FriendWeight = 80.5
 var height = 1.5
 
@@ -38,39 +38,45 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
-var isKuwaiti = false
+var firstName1 = "Zainab"
+var lastName1 = "Ali"
+var email1 = "zoza.3li@hotmail.com"
+var phoneNumber1 = "55402035"
+var age1 = 18
+var country1 = "Kuwait"
+var area1 = "Bayan"
+var password1 = "18102001"
+var isKuwaiti1 = true
 
 //Person 2
-
-
-
-
-
-
+var firstName2 = "Hanan"
+var lastName2 = "Dashti"
+var email2 = "hanan.dashti@gmail.com"
+var phoneNumber2 = "99557262"
+var age2 = 49
+var country2 = "Kuwait"
+var area2 = "Bayan"
+var password2 = "2181972"
+var isKuwaiti2 = true
 
 
 
 
 //Person 3
-
-
-
-
-
-
-
+var firstName3 = "Abyar"
+var lastName3 = "Ali"
+var email3 = "Dr.abyar.ali@gmail.com"
+var phoneNumber3 = "55050423"
+var age3 = 24
+var country3 = " Kuwait"
+var area3 = "Bayan"
+var password3 = "2281995"
+var isKuwaiti3 = true
 
 
 /*
 - - - -
+
 # المطلوب الثالث
 * قم بمقارنة معلومات كل شخص مع الشخص الاخر
  * قارن الاعمار اذا كانت متساوية ام لا
@@ -82,3 +88,103 @@ var isKuwaiti = false
 
  
  */
+    
+firstName1 == firstName2
+firstName1 == firstName3
+firstName2 == firstName3
+firstName1 != firstName2
+firstName1 != firstName3
+firstName2 != firstName3
+
+lastName1 == lastName2
+lastName1 == lastName3
+lastName2 == lastName3
+lastName1 != lastName2
+lastName1 != lastName3
+lastName2 != lastName3
+
+email1 == email2
+email1 == email3
+email2 == email3
+email1 != email2
+email1 != email3
+email2 != email3
+
+phoneNumber1 == phoneNumber2
+phoneNumber1 == phoneNumber3
+phoneNumber2 == phoneNumber3
+phoneNumber1 != phoneNumber2
+phoneNumber1 != phoneNumber3
+phoneNumber2 != phoneNumber3
+
+country1 == country2
+country1 == country3
+country2 == country3
+country1 != country2
+country1 != country3
+country2 != country3
+
+area1 == area2
+area1 == area3
+area2 == area3
+area1 != area2
+area1 != area3
+area2 != area3
+
+password1 == password2
+password1 == password3
+password2 == password3
+password1 != password2
+password1 != password3
+password2 != password3
+
+isKuwaiti1 == isKuwaiti2
+isKuwaiti1 == isKuwaiti3
+isKuwaiti2 == isKuwaiti3
+isKuwaiti3 != isKuwaiti2
+isKuwaiti1 != isKuwaiti3
+isKuwaiti1 != isKuwaiti2
+
+age1 == age2
+age1 == age3
+age2 == age3
+age1 != age2
+age1 != age3
+age2 != age3
+age1 > age2
+age1 > age3
+age2 > age3
+age1 >= age2
+age1 >= age3
+age2 >= age3
+age1 < age2
+age1 < age3
+age2 < age3
+age1 <= age2
+age1 <= age3
+age2 <= age3
+
+18 < age1
+18 < age2
+18 < age3
+
+18 > age1
+18 > age2
+18 > age3
+
+age1 == 17
+age2 == 17
+age3 == 17
+
+age1 != 17
+age2 != 17
+age3 != 17
+
+
+
+
+
+
+
+
+
